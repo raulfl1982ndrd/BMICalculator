10bf3a68f9401f886b1892824f1b8b82ac767d6c
raulfl1982ndrd <raulfl1982ndrd@gmail.com>
2024-06-04 14:01:41 +0200

    Primeros pasos en Android

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.bmicalculator
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var welcomeTextView : TextView
+    lateinit var  clickButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        //Va a la vista del activity y busca el componentes visual
+        // tenemos la referencia al componente visual
+        welcomeTextView = findViewById(R.id.welcomeTextView)
+        clickButton = findViewById(R.id.clickButton)
+        //CAmbiamos el texto y al hacer click cambiamos el e¡texto otra vez
+        welcomeTextView.text = getString(R.string.main_activity_welcome_text)
+        clickButton.setOnClickListener{
+            welcomeTextView.text = getString(R.string.main_activity_click_text)
+            welcomeTextView.setBackgroundColor(Color.parseColor("#FF0000"))
+            clickButton.setBackgroundColor(Color.parseColor("#FF0000"));
+        };
+
     }
 }
