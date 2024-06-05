d40f0b108ea7a91923c5ff73cc574e1cd2bdced7
raulfl1982ndrd <raulfl1982ndrd@gmail.com>
2024-06-05 14:01:26 +0200

    Aplicacion de IMC

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -2,30 +2,103 @@ package com.example.bmicalculator
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
-    lateinit var welcomeTextView : TextView
-    lateinit var  clickButton: Button
+    lateinit var heightEditText : EditText
+    lateinit var weightTextView : TextView
+    lateinit var  minusButton: Button
+    lateinit var  addButton: Button
+    lateinit var descriptionTextview :TextView
+    lateinit var resultTextView:TextView
+    lateinit var  calculateButton: Button
+
+    var height: Int = 120
+    var weight: Int = 70
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Va a la vista del activity y busca el componentes visual
-        // tenemos la referencia al componente visual
-        welcomeTextView = findViewById(R.id.welcomeTextView)
-        clickButton = findViewById(R.id.clickButton)
-        //CAmbiamos el texto y al hacer click cambiamos el e¡texto otra vez
-        welcomeTextView.text = getString(R.string.main_activity_welcome_text)
-        clickButton.setOnClickListener{
-            welcomeTextView.text = getString(R.string.main_activity_click_text)
-            welcomeTextView.setBackgroundColor(Color.parseColor("#FF0000"))
-            clickButton.setBackgroundColor(Color.parseColor("#FF0000"));
-        };
+        //Log.i(tag:"IMC",msg:"Se ha creado el MainActivity")
+        heightEditText = findViewById(R.id.heightEditText)
+        weightTextView = findViewById(R.id.weightTextView)
+        minusButton = findViewById(R.id.minusButton)
+        addButton = findViewById(R.id.plusButton)
+        descriptionTextview = findViewById(R.id.descripcionTextView)
+        resultTextView = findViewById(R.id.resultTextView)
+        calculateButton = findViewById(R.id.calculateButton)
+        setHeight()
+        setWeight()
+        minusButton.setOnClickListener{
+            weight--
+            setWeight()
+            //Log.i(tag:"IMC",msg:"He reducido el peso")
+        }
+        addButton.setOnClickListener{
+            weight++
+            setWeight()
+            //Log.i(tag:"IMC",msg:"He aumentado el peso")
+        }
+        calculateButton.setOnClickListener {
+            height = heightEditText.text.toString().toInt()
+            var result = weight / (height / 100f).pow(2)
+            var descripcion: String = "IMC"
+
+            if (result < 16.5) {
+                descripcion = "Bajo peso severo"
+                descriptionTextview.setTextColor(Color.parseColor("#FF0000"))
+                resultTextView.setTextColor(Color.parseColor("#FF0000"))
+            }
+            else if ((result >= 16.5) && (result < 18.5)){
+                descripcion = "Bajo peso"
+                descriptionTextview.setTextColor(Color.parseColor("#FF6C00"))
+                resultTextView.setTextColor(Color.parseColor("#FF6C00"))
+            }
+            else if ((result >= 18.5) && (result <25)){
+                descripcion = "Peso normal"
+                descriptionTextview.setTextColor(Color.parseColor("#00FF78"))
+                resultTextView.setTextColor(Color.parseColor("#00FF78"))
+            }
+            else if ((result >= 25) && (result <30)){
+                descripcion = "Sobrepeso"
+                descriptionTextview.setTextColor(Color.parseColor("#FF6C00"))
+                resultTextView.setTextColor(Color.parseColor("#FF6C00"))
+            }
+            else if ((result >= 30) && (result <35)) {
+                descripcion = "Obesidad tipo 1 (moderada)"
+                descriptionTextview.setTextColor(Color.parseColor("#FF2E00"))
+                resultTextView.setTextColor(Color.parseColor("#FF2E00"))
+            }
+            else if ((result >= 35) && (result <40)) {
+                descripcion = "Obesidad tipo 2 (severa)"
+                descriptionTextview.setTextColor(Color.parseColor("#FF2300"))
+                resultTextView.setTextColor(Color.parseColor("#FF2300"))
+            }
+            else if ((result >= 40)) {
+                descripcion = "Obesidad tipo 3 (mórbida)"
+                descriptionTextview.setTextColor(Color.parseColor("#FF0000"))
+                resultTextView.setTextColor(Color.parseColor("#FF0000"))
+            }
+            descriptionTextview.text = descripcion.toString()
+            resultTextView.text = result.toString()
+        }
+
+    };
+
+
+    fun setHeight(){
+        heightEditText.setText(height.toString())
+    }
+    fun setWeight(){
+        weightTextView.text = "$weight Kg"
+    }
 
     }
-}
