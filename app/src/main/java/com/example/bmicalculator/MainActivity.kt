8a32883304d00afc258d9a19e6c3bcca48bc19e6
rfl1982 <raulfl1982unity@gmail.com>
2025-05-15 21:29:07 +0200

    Se solucionan problemas y se añade imagen descriptiva del estado del peso

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,20 +1,16 @@
 package com.example.bmicalculator
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.icu.text.DecimalFormat
-import android.location.GnssAntennaInfo.Listener
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.slider.Slider
-import com.google.android.material.slider.Slider.OnChangeListener
 import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
@@ -33,6 +29,7 @@ class MainActivity : AppCompatActivity() {
     var height: Int = 170
     var weight: Int = 70
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -47,12 +44,12 @@ class MainActivity : AppCompatActivity() {
         resultTextView = findViewById(R.id.resultTextView)
         calculateButton = findViewById(R.id.calculateButton)
         estadoTextView = findViewById(R.id.estadoTextView)
-        imageViewPanel = findViewById(R.id.imageViewPanel)
+        imageViewPanel = findViewById(R.id.imagenImageView)
         setHeight()
         setWeight()
         //Barra de titulo de la aplicacion
-        supportActionBar?.title = "Mi calculadores de IMC"
-        supportActionBar?.subtitle = "Una descripcion cualquiera"
+        supportActionBar?.title = "Calculadora de IMC"
+        supportActionBar?.subtitle = "Revisa tu IMC"
 
         //heightSlider.addOnChangeListener(heightSlider)
         heightSlider.value = height.toFloat()
@@ -103,42 +100,50 @@ class MainActivity : AppCompatActivity() {
                 resultTextView.setTextColor(Color.parseColor("#FF0000"))
                 estadoTextView.text = "Aumente su peso"
                 //imageViewPanel.setImageResource()
+                imageViewPanel.setImageDrawable  (getDrawable(R.drawable.extremadelgadez))
             }
             else if ((result >= 16.5) && (result < 18.5)){
                 descripcion = "Bajo peso"
                 descriptionTextview.setTextColor(Color.parseColor("#FF6C00"))
                 resultTextView.setTextColor(Color.parseColor("#FF6C00"))
                 estadoTextView.text = "Aumente su peso"
+                imageViewPanel.setImageDrawable  (getDrawable(R.drawable.extremadelgadez))
             }
             else if ((result >= 18.5) && (result <25)){
                 descripcion = "Peso normal"
                 descriptionTextview.setTextColor(Color.parseColor("#00FF78"))
                 resultTextView.setTextColor(Color.parseColor("#00FF78"))
                 estadoTextView.text = "Peso correcto"
+                imageViewPanel.setImageDrawable  (getDrawable(R.drawable.pesosaludable))
+
             }
             else if ((result >= 25) && (result <30)){
                 descripcion = "Sobrepeso"
                 descriptionTextview.setTextColor(Color.parseColor("#FF6C00"))
                 resultTextView.setTextColor(Color.parseColor("#FF6C00"))
                 estadoTextView.text = "Reduzca su peso"
+                imageViewPanel.setImageDrawable  (getDrawable(R.drawable.obesidad))
             }
             else if ((result >= 30) && (result <35)) {
                 descripcion = "Obesidad tipo 1 (moderada)"
                 descriptionTextview.setTextColor(Color.parseColor("#FF2E00"))
                 resultTextView.setTextColor(Color.parseColor("#FF2E00"))
                 estadoTextView.text = "Consulte con su medico"
+                imageViewPanel.setImageDrawable  (getDrawable(R.drawable.obesidad))
             }
             else if ((result >= 35) && (result <40)) {
                 descripcion = "Obesidad tipo 2 (severa)"
                 descriptionTextview.setTextColor(Color.parseColor("#FF2300"))
                 resultTextView.setTextColor(Color.parseColor("#FF2300"))
                 estadoTextView.text = "Consulte con su medico"
+                imageViewPanel.setImageDrawable  (getDrawable(R.drawable.obesidad))
             }
             else if ((result >= 40)) {
                 descripcion = "Obesidad tipo 3 (mórbida)"
                 descriptionTextview.setTextColor(Color.parseColor("#FF0000"))
                 resultTextView.setTextColor(Color.parseColor("#FF0000"))
                 estadoTextView.text = "Consulte con su medico"
+                imageViewPanel.setImageDrawable  (getDrawable(R.drawable.obesidad))
             }
             descriptionTextview.text = descripcion.toString()
             //Limitar el numero de decimales que muestra el resultTextView
@@ -160,4 +165,4 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    }
+}
